25dbe2384e8d39252d4f7c3569f8c69a27548e3b
nicos <yenialexandra7valan@gmail.com>
2025-04-22 22:50:26 -0500

    primer commit

---
+++ src/main/java/co/edu/umanizales/myfirstapi/controller/LocationController.java
@@ -0,0 +1,61 @@
+package co.edu.umanizales.myfirstapi.controller;
+
+import co.edu.umanizales.myfirstapi.model.Location;
+import co.edu.umanizales.myfirstapi.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/location")
+public class LocationController {
+    /*
+        @GetMapping
+        public Location getLocation(){
+            Location location = new Location("1","Manizales");
+
+            return location;
+        }
+
+        Método "get" para devolver una lista de Location
+        @GetMapping
+        public List<Location> getLocations() {
+            // Aquí estamos creando una lista de ejemplo. Podrías obtener estos datos de una base de datos.
+            return Arrays.asList(
+                    new Location("001", "Location 1"),
+                    new Location("002", "Location 2"),
+                    new Location("003", "Location 3")
+            );
+        }
+
+     */
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping
+    public List<Location> getLocations() {
+
+        return locationService.getLocations();
+
+    }
+
+    @GetMapping(path = "/{code}")
+    public Location getLocationByCode(@PathVariable String code){
+        return locationService.getLocationByCode(code);
+    }
+
+    @GetMapping(path = "/states")
+    public List<Location> getLocationsByStates(){
+        return locationService.getStates();
+    }
+
+}
---
+++ src/main/java/co/edu/umanizales/myfirstapi/service/LocationService.java
@@ -0,0 +1,82 @@
+package co.edu.umanizales.myfirstapi.service;
+
+import co.edu.umanizales.myfirstapi.model.Location;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Getter
+
+public class LocationService {
+
+    private List<Location> locations;
+
+    @Value( "${locations_filename}" )
+    private String locationsFilename;
+
+    @PostConstruct
+    public void readLocationsFromCSV() throws IOException, URISyntaxException {
+        locations = new ArrayList<>();
+        locations.add(new Location("05","ANTIOQUIA"));
+        locations.add(new Location("17","CALDAS"));
+        locations.add(new Location("66","RISARALDA"));
+
+        Path pathFile = Paths.get(ClassLoader.getSystemResource(locationsFilename).toURI());
+
+        try (BufferedReader br = new BufferedReader(new FileReader(pathFile.toString()))) {
+
+            String[] tokens;
+
+            String line;
+            boolean firstLine = true;
+            // Leer todas las filas del CSV
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
+
+                // Crear un nuevo objeto Location y agregarlo a la lista
+                tokens = line.split(",");
+                String code = tokens[2];
+                String name = tokens[3];
+
+                locations.add(new Location(code, name));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;  // Lanza la excepción para que pueda manejarse en la capa superior si es necesario
+        }
+    }
+
+    public Location getLocationByCode(String code) {
+        for (Location location : locations) {
+            if (location.getCode().equals(code)) {
+                return location;
+            }
+        }
+        return null;
+    }
+
+    public List<Location> getStates() {
+        List<Location> states = new ArrayList<>();
+        for (Location location : locations) {
+            if(location.getCode().length() ==2){
+                states.add(location);
+            }
+        }
+        return states;
+    }
+
+}
